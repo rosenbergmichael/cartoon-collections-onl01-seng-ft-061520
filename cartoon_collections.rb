046927221578bf35712b98e181a004e326f5eae0
Michael Rosenberg <rosenbergmj1@gmail.com>
2020-06-18 19:05:01 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,10 +1,10 @@
 def roll_call_dwarves(array) 
   dwarves=[]
   array.each_with_index do |name|
-  dwarves << puts "#{name}""
-end
-dwarves 
-end
+  dwarves << puts "#{name}"
+end 
+dwarves
+end 
 
 def summon_captain_planet# code an argument here
   # Your code here

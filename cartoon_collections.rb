aa367aeddffe11a8824eeb8e02f9877e9d12d4c4
Michael Rosenberg <rosenbergmj1@gmail.com>
2020-06-18 21:28:03 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -13,10 +13,12 @@ calls
 end 
 
 
-
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls(array)
   # Your code here
 end
+
+
+
 
 def find_the_cheese# code an argument here
   # the array below is here to help
